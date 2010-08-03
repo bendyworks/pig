e3d177411ab2b4e4cc17bff11a430441cc4c849d
Jim Remsik <bigtiger@hashrocket.com>
2010-08-03 14:06:18 -0400

    Add Rails 3 generators

---
+++ lib/generators/pig/pig_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators'
+
+class PigGenerator < Rails::Generators::Base
+
+  def install_pig
+    gem("pig")
+    route("match '/revision', :to => Pig.new")
+  end
+
+  def self.source_root
+    File.join(File.dirname(__FILE__), 'templates')
+  end
+
+end
