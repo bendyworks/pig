0e95d81c3790f2d38801e891ff32212eedfc3176
Jim Remsik <bigtiger@hashrocket.com>
2010-08-20 16:19:28 -0400

    Developer Generates Custom Route For Revision Page
    [close #2]

--- lib/generators/pig/pig_generator.rb
+++ lib/generators/pig/pig_generator.rb
@@ -2,6 +2,7 @@ require 'rails/generators'
 
 class PigGenerator < Rails::Generators::Base
 
+  class_option :revision_path, :default => '/revision'
   class_option :skip_routes,
     :type => :boolean,
     :default => false,
@@ -10,11 +11,20 @@ class PigGenerator < Rails::Generators::Base
   def install_pig
     if options[:skip_routes]
       puts "Nothing to do"
+    else
+      route("match '#{formatted_revision_path}', :to => Pig.new")
     end
   end
 
   def self.source_root
     File.join(File.dirname(__FILE__), 'templates')
+  end
+
+  protected
+
+  def formatted_revision_path
+    path = options[:revision_path]
+    path.starts_with?('/') ? path : "/#{path}"
   end
 
 end
